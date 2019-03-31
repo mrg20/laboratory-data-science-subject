89253c2cfd1b23eb52ffc9cdead2c59bd95db3e0
Alba Lamas <alba.v.lamas@gmail.com>
2019-03-31 23:22:23 +0200

    added the create directoris py

--- src/data/create_directories.py
+++ src/data/create_directories.py
@@ -2,18 +2,11 @@ import os
 
 # define the name of the directory to be created
 cryptocurrency_raw='../../data/raw/cryptocurrency'
-iris_raw='../data/raw/iris'
-measures_raw='../data/raw/measures'
+iris_raw='../../data/raw/iris'
+measures_raw='../../data/raw/measures'
 cryptocurrency_processed='../../data/processed/cryptocurrency'
-iris_processed='../data/processed/iris'
-measures_processed='../data/processed/measures'
+iris_processed='../../data/processed/iris'
+measures_processed='../../data/processed/measures'
 
-try:
-    os.makedirs('../../data/raw/')
-    os.makedirs('../../data/processed/')
-    for dr in [cryptocurrency_raw, iris_raw, measures_raw, cryptocurrency_processed, iris_processed, measures_processed]:
-        os.mkdir(dr)
-except OSError:  
-    print ("Creation of the directory failed ", dr)
-else:  
-    print ("Successfully created the directory")
+for dr in [cryptocurrency_raw, iris_raw, measures_raw, cryptocurrency_processed, iris_processed, measures_processed]:
+    os.makedirs(dr, exist_ok=True)
